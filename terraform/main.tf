e66cb90bc4046c979b69d233ec1833ee21199295
driofrioCUNEF <diego.riofrio@cunef.edu>
2025-04-10 18:02:34 +0200

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,21 @@ resource "google_vpc_access_connector" "vpc_connector" {
   region        = var.region
   network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.8.0.0/28"
-  max_throughput = 300  # Mbps
+  max_throughput = 200
 }
 
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "private-ip-alloc"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+  project       = var.project_id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+  depends_on              = [google_compute_global_address.private_ip_alloc]
+}
