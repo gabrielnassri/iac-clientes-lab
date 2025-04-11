293e20356f22c3b1e9d68c8e6f454d6082bf5651
driofrioCUNEF <diego.riofrio@cunef.edu>
2025-04-11 19:44:47 +0200

    Update cloudrun.tf

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -1,8 +1,21 @@
+resource "google_artifact_registry_repository" "gcr_compat" {
+  location      = var.region
+  repository_id = "gcr-compat"
+  format        = "DOCKER"
+  description   = "Repositorio Docker compatible con gcr.io para clientes-api"
+
+  docker_config {
+    immutable_tags = false
+  }
+}
+
 resource "google_cloud_run_service" "java_api" {
   name     = "clientes-api"
   location = var.region
   project  = var.project_id
 
+  depends_on = [google_artifact_registry_repository.gcr_compat]
+  
   template {
     metadata {
       annotations = {
