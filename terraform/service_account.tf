c3af71d74ed262ef5fbf0988547044c434f3a7e3
driofrioCUNEF <diego.riofrio@cunef.edu>
2025-04-11 13:45:50 +0200

    Update service_account.tf

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -27,6 +27,11 @@ resource "google_project_iam_member" "run_invoker" {
   member = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
+resource "google_project_iam_member" "artifact_registry_writer" {
+  role   = "roles/artifactregistry.writer"
+  member = "serviceAccount:${google_service_account.github_deployer.email}"
+}
+
 resource "google_service_account_key" "github_key" {
   service_account_id = google_service_account.github_actions.name
   keepers = {
