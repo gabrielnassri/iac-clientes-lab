35160260d3b52b002f32d65f2f1fa15e7bfda179
driofrioCUNEF <diego.riofrio@cunef.edu>
2025-04-11 23:05:51 +0200

    Update firewall.tf

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -11,3 +11,18 @@ resource "google_compute_firewall" "allow_http" {
   target_tags   = ["frontend"]
   description   = "Permitir tráfico HTTP a la VM"
 }
+
+
+resource "google_compute_firewall" "allow_ssh_from_gcp" {
+  name    = "allow-ssh-from-gcp"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"] # Rango oficial de IAP y Cloud Shell
+  target_tags   = ["frontend"]
+  description   = "Permitir SSH solo desde infraestructura de GCP como Cloud Shell e IAP"
+}
